dd5181b5423e59683bfb153bf3f31df79bf0bc15
gradhav001 <jesse.richard@strathmore.edu>
2023-10-17 07:34:36 +0300

    solved the issue of excesive output

--- src/Arrays/Target.java
+++ src/Arrays/Target.java
@@ -10,5 +10,28 @@ package Arrays;
  * @author user1
  */
 public class Target {
-    
+
+    public void targetfinder(int[] num, int target) {
+        for (int i = 0; i < num.length; i++) {
+            for (int j = i + 1; j < num.length; j++) {
+                int evaluation = num[i] + num[j];
+                if (evaluation == target) {
+                    System.out.println("[" + num[i] + "," + num[j] + "]");
+                } else {
+//                    System.out.println("the target could not be reached");
+                    break;
+                }
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Target t = new Target();
+        int[] array = {1, 2, 3, 4};
+        int target = 7;
+        t.targetfinder(array, target);
+  
+    }
+
 }
--- src/Arrays/search.java
+++ src/Arrays/search.java
@@ -22,7 +22,7 @@ public class search {
     public void SearchSolution() {
         int[] array = new int[15];
         Random random = new Random();
-
+    
         for (int i = 0; i < array.length; i++) {
             int randomNumber = random.nextInt(20);
             array[i] = randomNumber;
